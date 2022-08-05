799d877b75fbe0671e843736c321c8ec22777b81
Strelkova Olga <strelod@yandex.ru>
2022-08-05 09:20:47 +0300

    feat lang ru

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,19 @@
-export default class MyDocument extends Document {}
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
